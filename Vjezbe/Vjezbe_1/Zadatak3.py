81f96cf75dbdaf5c4022762341e462d0c340f182
unknown <mia@oberan.com>
2022-03-04 10:55:58 +0100

    Zadatak 3

---
+++ Vjezbe/Vjezbe_1/Zadatak3.py
@@ -0,0 +1,6 @@
+x1 = int(input("Unesite x koordinatu za prvu tocku"))
+y1 = int(input("Unesite y kooridnatu za prvu tocku"))
+x2 = int(input("Unesite x koordinatu za drugu tocku"))
+y2 = int(input("Unesite y koordinatu za drugu tocku"))
+
+    
