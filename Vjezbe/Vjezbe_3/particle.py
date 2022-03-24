7ad09df960315d294228bd3c82f48101409f04b2
unknown <mia@oberan.com>
2022-03-24 09:25:33 +0100

    commit

--- Vjezbe/Vjezbe_3/particle.py
+++ Vjezbe/Vjezbe_3/particle.py
@@ -1,6 +1,6 @@
 import math 
 class Particle():
-    def __init__(self):
+    def __init__(self,x0,y0,v0,theta):
         self.x = []
         self.y = []
         self.v_x = []
@@ -13,14 +13,21 @@ class Particle():
     def set_initial_conditions(self, x0,y0,v0,theta):
         self.a_x.append(0)
         self.a_y.append(9.81)
-        self.v_x.append(v0*mat.cos(math.radians(theta)))
+        self.v_x.append(v0*math.cos(math.radians(theta)))
         self.v_y.append(v0*math.sin(math.radians(theta)))
         self.t.append(0)
         self.x.append(x0)
         self.y.append(y0)
     
     def reset(self):
-        self.__init__()
+        self.x = []
+        self.y = []
+        self.v_x = []
+        self.v_y = []
+        self.t = []
+        self.a_x = []
+        self.a_y = []
+        self.dt = 0.1
     
     def __move(self):
         self.t.append(self.t[-1]+self.dt)
@@ -35,6 +42,17 @@ class Particle():
     def range(self):
         while self.y[-1] > 0:
             self.__move()
-    return self.x[-1]
+        return self.x[-1]
+
+    def PrintInfo(self):
+        print("v_y je {}".format (self.v_y))
+        print("a_x je {}".format(self.a_x))
+        print("v_x je {}".format(self.v_x))
+
+
 
 p1 = Particle(0,0,15,40)
+p1.set_initial_conditions(0,0,15,40)
+p1.PrintInfo()
+p1.reset()
+p1.PrintInfo()
