bbbb77617fc67b28e50e48f1599423313b5f2b9d
unknown <mia@oberan.com>
2022-03-25 10:52:05 +0100

    deriviranje

--- Vjezbe/Vjezbe_4/calculus.py
+++ Vjezbe/Vjezbe_4/calculus.py
@@ -1,4 +1,12 @@
 def derivacija(func,x):
     dx = 0.1
-    derivacija = (func(x+dx)-func(x)//dx))
+    derivacija = (func(x+dx)-func(x)//dx)
     return derivacija
+
+def f1(x):
+    return x*x
+
+def f1(x):
+    return 6*x**4 + 5*x + 3
+
+def derivacija(f1,1)
